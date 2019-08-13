25fedf10bab811edef4375d5b350156c2fe25904
Michael <michael.rincon@javeriana.edu.co>
2019-08-12 21:39:11 -0500

    Cambio de color

--- funcionamiento.js
+++ funcionamiento.js
@@ -1,37 +1,16 @@
-function fondoSecLeave(){
-    var bienvenido=document.getElementById('bienveni');
-    var datosbio=document.getElementById('datosbio');
-    var experiencia=document.getElementById('expe');
-    var interesante=document.getElementById('inter');
-    if(bienvenido){
-        bienvenido.style.backgroundColor="white";
-        bienvenido.style.border="white";
-        bienvenido.style.borderColor="white"
-        bienvenido.style.padding="20px";
-    }else if(datosbio){
-        datosbio.style.backgroundColor="white";
-        datosbio.style.border="white";
-        datosbio.style.borderColor="white"
-        datosbio.style.padding="20px";
-    }
+/*Funcion mouseOver seccion*/
+function fondoSecOver(sec){
+    sec.style.backgroundColor="cyan";
+    sec.style.border="solid";
+    sec.style.borderColor="black"
+    sec.style.padding="10px";
 
 }
-function fondoSecOver(){
-    var bienvenido=document.getElementById('bienveni');
-    var datosbio=document.getElementById('datosbio');
-    var experiencia=document.getElementById('expe');
-    var interesante=document.getElementById('inter');
-    if(bienvenido){
-        bienvenido.style.backgroundColor="cyan";
-        bienvenido.style.border="solid";
-        bienvenido.style.borderColor="black"
-        bienvenido.style.padding="10px";
-    }else if(datosbio){
-        datosbio.style.backgroundColor="cyan";
-        datosbio.style.border="solid";
-        datosbio.style.borderColor="black"
-        datosbio.style.padding="10px";
-    }
 
-
+/*Funcion mouseOut seccion*/
+function fondoSecOut(sec){
+    sec.style.backgroundColor="white";
+    sec.style.border="white";
+    sec.style.borderColor="white"
+    sec.style.padding="20px";
 }
